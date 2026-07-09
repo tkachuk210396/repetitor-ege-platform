6fc6c8825a495c8b07adc450db0c13100293e162
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:24:08 +0400

    Create StudentProgress.js

---
+++ js/modules/student/StudentProgress.js
@@ -0,0 +1,9 @@
+// Мой прогресс
+export default class StudentProgress {
+    render() {
+        return `<div>
+            <h2>Мой прогресс</h2>
+            <p>Ваша статистика</p>
+        </div>`;
+    }
+}
