253904398227c7f9b3a6b315d6759993db45b8c2
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:22:27 +0400

    Create AdminTasks.js

---
+++ js/modules/admin/AdminTasks.js
@@ -0,0 +1,9 @@
+// Управление заданиями
+export default class AdminTasks {
+    render() {
+        return `<div>
+            <h2>Все задания</h2>
+            <button>Создать задание</button>
+        </div>`;
+    }
+}
