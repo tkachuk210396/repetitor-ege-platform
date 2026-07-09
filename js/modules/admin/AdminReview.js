76a549d28dacc8b5d64b509c45a057430657c41d
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:22:45 +0400

    Create AdminReview.js

---
+++ js/modules/admin/AdminReview.js
@@ -0,0 +1,9 @@
+// Проверка заданий
+export default class AdminReview {
+    render() {
+        return `<div>
+            <h2>Проверка работ</h2>
+            <p>Список работ на проверку</p>
+        </div>`;
+    }
+}
