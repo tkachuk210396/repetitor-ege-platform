f66cfdbac12a6e84e576888bc5a91b37f15574e2
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:23:51 +0400

    Create StudentTasks.js

---
+++ js/modules/student/StudentTasks.js
@@ -0,0 +1,9 @@
+// Мои задания
+export default class StudentTasks {
+    render() {
+        return `<div>
+            <h2>Мои задания</h2>
+            <p>Список ваших заданий</p>
+        </div>`;
+    }
+}
