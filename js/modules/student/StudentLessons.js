a11b547867b9c1ad71f5cb50712a5c2af0bc8e8d
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:23:35 +0400

    Create StudentLessons.js

---
+++ js/modules/student/StudentLessons.js
@@ -0,0 +1,9 @@
+// Мои уроки
+export default class StudentLessons {
+    render() {
+        return `<div>
+            <h2>Мои уроки</h2>
+            <p>Список ваших занятий</p>
+        </div>`;
+    }
+}
