e9d91def5eca38be4d811c669c1101086e5082f7
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:23:19 +0400

    Create StudentDashboard.js

---
+++ js/modules/student/StudentDashboard.js
@@ -0,0 +1,9 @@
+// Главная страница ученика
+export default class StudentDashboard {
+    render() {
+        return `<div>
+            <h1>Моя панель</h1>
+            <p>Добро пожаловать, ученик!</p>
+        </div>`;
+    }
+}
