335617c86022173effe09192e515cb5cb18157f4
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:25:51 +0400

    Create initData.js

---
+++ data/initData.js
@@ -0,0 +1,9 @@
+// Тестовые данные
+export const testUsers = [
+    { id: 1, name: 'Админ', email: 'admin@ege.ru', role: 'admin' },
+    { id: 2, name: 'Анна', email: 'anna@student.ru', role: 'student' }
+];
+
+export const testTasks = [
+    { id: 1, title: 'Задание 1', description: 'Описание задания' }
+];
