040364e993bda927a87bbea981ec33bae82c90d6
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 14:21:52 +0400

    Create AdminDashboard.js

---
+++ js/modules/admin/AdminDashboard.js
@@ -0,0 +1,9 @@
+// Главная страница администратора
+export default class AdminDashboard {
+    render() {
+        return `<div class="admin-panel">
+            <h1>Панель администратора</h1>
+            <p>Добро пожаловать!</p>
+        </div>`;
+    }
+}
