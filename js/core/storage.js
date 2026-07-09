a5458465643aa6397a971cfdec8887f9fb0abd68
tkachuk210396 <tkachuk210396@gmail.com>
2026-07-09 15:07:26 +0400

    Update storage.js

--- js/core/storage.js
+++ js/core/storage.js
@@ -1,10 +1,29 @@
-// Работа с хранилищем данных
+// ============================================
+// МОДУЛЬ РАБОТЫ С ХРАНИЛИЩЕМ
+// ============================================
+
 export default class Storage {
     get(key) {
-        return JSON.parse(localStorage.getItem(key)) || [];
+        try {
+            return JSON.parse(localStorage.getItem(key)) || [];
+        } catch {
+            return [];
+        }
     }
     
     set(key, data) {
         localStorage.setItem(key, JSON.stringify(data));
+    }
+    
+    getItem(key) {
+        return localStorage.getItem(key);
+    }
+    
+    setItem(key, value) {
+        localStorage.setItem(key, value);
+    }
+    
+    remove(key) {
+        localStorage.removeItem(key);
     }
 }
